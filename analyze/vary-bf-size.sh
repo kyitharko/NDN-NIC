58f227795122ca608f01861caebb18b528f19459
Junxiao Shi <git@mail1.yoursunny.com>
2016-01-05 17:03:02 +0000

    analyze: vary-bf-size varies BF3 size

--- analyze/vary-bf-size.sh
+++ analyze/vary-bf-size.sh
@@ -2,7 +2,7 @@
 # Plot false positive rate as a function of Bloom filter size.
 # Usage: ./vary-bf-size.sh key sizes params..
 #   key: prefix of output file names
-#   sizes: a TSV file where each line is a tuple of BF1,BF2 size
+#   sizes: a TSV file where each line is a tuple of BF1,BF2,BF3 size
 #   params: passed to nicsim.py
 
 R="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
@@ -20,6 +20,8 @@ shift 2
   echo -ne '\t'
   echo -n bf2size
   echo -ne '\t'
+  echo -n bf3size
+  echo -ne '\t'
   echo -n host
   echo -ne '\t'
   echo -n nNicAccepts
@@ -33,8 +35,9 @@ shift 2
 while read -r -a SIZES; do
   BF1SIZE=${SIZES[0]}
   BF2SIZE=${SIZES[1]}
-  KEY1=$KEY.bf1-${BF1SIZE}_bf2-${BF2SIZE}
-  $R/analyze/one.sh $KEY1 --bf1=$BF1SIZE --bf2=$BF2SIZE "$@" > /dev/null
+  BF3SIZE=${SIZES[2]}
+  KEY1=$KEY.bf1-${BF1SIZE}_bf2-${BF2SIZE}_bf3-${BF3SIZE}
+  $R/analyze/one.sh $KEY1 --bf1=$BF1SIZE --bf2=$BF2SIZE --bf3=$BF2SIZE "$@" > /dev/null
 
   awk '
   BEGIN {
@@ -45,11 +48,11 @@ while read -r -a SIZES; do
   NR > 1 {
     totalNicAccepts += $4
     totalFalsePositives += $5
-    print '$BF1SIZE', '$BF2SIZE', $1, $4, $5,
+    print '$BF1SIZE', '$BF2SIZE', '$BF3SIZE', $1, $4, $5,
           $4==0 ? 0 : $5/$4
   }
   END {
-    print '$BF1SIZE', '$BF2SIZE', "+", totalNicAccepts, totalFalsePositives,
+    print '$BF1SIZE', '$BF2SIZE', '$BF3SIZE', "+", totalNicAccepts, totalFalsePositives,
           totalNicAccepts==0 ? 0 : totalFalsePositives/totalNicAccepts
   }
   ' $KEY1.quick-analyze.tsv
