d1246b86fefce1eda1e8f3329fddf4b90d93eac8
Junxiao Shi <git@mail1.yoursunny.com>
2016-01-08 00:20:48 +0000

    nicsim: AitCs PMFP reduction

--- nicsim/ait.py
+++ nicsim/ait.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # A copy of the GNU Lesser General Public License is in the file COPYING.
 
+import sys
 import nameutil
 from name_tree import NameTree, NameTreeNode
 
@@ -45,36 +46,44 @@ class AitNode(NameTreeNode):
         self.deepestMultiCs1Ptr = None
         """reference of the subtree containing deepest CS2 key with two or more CS1 descendants"""
 
+        self.shallowestCs1Dist = None
+        """distance to shallowest CS1 key"""
+        self.shallowestCs1Ptr = None
+        """reference of the subtree containing shallowest CS1 key"""
+
     def _printAttributes(self):
-        return "".join([
-                         "inCs " if self.inCs else "",
-                         "CS1 " if self.hasCs1 else "",
-                         "CS2 " if self.hasCs2 else "",
-                         "FIB1 " if self.hasFib1 else "",
-                       ] +
-                       ([
-                         "deep2=none " if self.deepestCs2Dist is None
-                         else "deep2=self " if self.deepestCs2Dist == 0
-                         else "deep2=(%d,%s) " % (self.deepestCs2Dist, nameutil.getLastComponent(self.deepestCs2Ptr.name)),
-                         "nCS1=%d " % self.nCs1Descendants,
-                         "deep1=none " if self.deepestMultiCs1Dist is None
-                         else "deep1=self " if self.deepestMultiCs1Dist == 0
-                         else "deep1=(%d,%s) " % (self.deepestMultiCs1Dist, nameutil.getLastComponent(self.deepestMultiCs1Ptr.name)),
-                       ] if self.hasCs2 else [])
-                      )
+        return "".join(
+            [
+              "inCs " if self.inCs else "",
+              "CS1 " if self.hasCs1 else "",
+              "CS2 " if self.hasCs2 else "",
+              "FIB1 " if self.hasFib1 else "",
+            ] +
+            ([
+              "deep2=none " if self.deepestCs2Dist is None
+              else "deep2=self " if self.deepestCs2Dist == 0
+              else "deep2=(%d,%s) " % (self.deepestCs2Dist, nameutil.getLastComponent(self.deepestCs2Ptr.name)),
+              "nCS1=%d " % self.nCs1Descendants,
+              "deep1=none " if self.deepestMultiCs1Dist is None
+              else "deep1=self " if self.deepestMultiCs1Dist == 0
+              else "deep1=(%d,%s) " % (self.deepestMultiCs1Dist, nameutil.getLastComponent(self.deepestMultiCs1Ptr.name)),
+              "shallow1=none " if self.shallowestCs1Dist is None
+              else "shallow1=(%d,%s) " % (self.shallowestCs1Dist, nameutil.getLastComponent(self.shallowestCs1Ptr.name)),
+             ] if self.hasCs2 else [])
+            )
 
     def labelCs2(self):
         """
         Add CS2 key for this node.
         """
         if self.hasCs2:
-            return
-        self.unlabelCs1()
+            return 0
+
         self.hasCs2 = True
         self.tree.bf2.add(self.name, "CS2")
         self.tree.nCs2 += 1
 
-        self._updateCs2Fields()
+        return self._updateCs2Fields()
 
     def unlabelCs2(self):
         """
@@ -89,11 +98,7 @@ class AitNode(NameTreeNode):
         self.tree.bf2.remove(self.name, "CS2")
         self.tree.nCs2 -= 1
 
-        self.deepestCs2Dist = None
-        self.deepestCs2Ptr = None
-        self.nCs1Descendants = 0
-        self.deepestMultiCs1Dist = None
-        self.deepestMultiCs1Ptr = None
+        self._clearCs2Fields()
 
         if self.parent is not None:
             self.parent._updateCs2Fields()
@@ -125,18 +130,27 @@ class AitNode(NameTreeNode):
         if self.parent is not None:
             self.parent._updateCs2Fields()
 
-    def _updateCs2Fields(self):
-        assert self.hasCs2
-
+    def _clearCs2Fields(self):
         self.deepestCs2Dist = 0
         self.deepestCs2Ptr = None
         self.nCs1Descendants = 0
         self.deepestMultiCs1Dist = None
         self.deepestMultiCs1Ptr = None
+        self.shallowestCs1Dist = None
+        self.shallowestCs1Ptr = None
+
+    def _updateCs2Fields(self):
+        """
+        Update shortcut fields on CS2 keys.
+        """
+        assert self.hasCs2
+        self._clearCs2Fields()
 
         for child in self.iterChildren():
             if child.hasCs1:
                 self.nCs1Descendants += 1
+                self.shallowestCs1Dist = 1
+                self.shallowestCs1Ptr = child
             if not child.hasCs2:
                 continue
             if child.deepestCs2Dist + 1 > self.deepestCs2Dist:
@@ -147,6 +161,10 @@ class AitNode(NameTreeNode):
                (self.deepestMultiCs1Dist is None or child.deepestMultiCs1Dist + 1 > self.deepestMultiCs1Dist):
                 self.deepestMultiCs1Dist = child.deepestMultiCs1Dist + 1
                 self.deepestMultiCs1Ptr = child
+            if child.shallowestCs1Dist is not None and \
+               (self.shallowestCs1Dist is None or child.shallowestCs1Dist + 1 < self.shallowestCs1Dist):
+                self.shallowestCs1Dist = child.shallowestCs1Dist + 1
+                self.shallowestCs1Ptr = child
 
         if self.nCs1Descendants >= 2 and self.deepestMultiCs1Dist is None:
             self.deepestMultiCs1Dist = 0
@@ -173,6 +191,16 @@ class AitNode(NameTreeNode):
         if self.deepestMultiCs1Dist == 0:
             return self
         return self.deepestMultiCs1Ptr.findReduction1()
+
+    def findReductionPmfp(self):
+        """
+        Find a target for PMFP reduction within this subtree.
+        """
+        if self.hasCs1:
+            return self
+        if self.shallowestCs1Dist is None:
+            return None
+        return self.shallowestCs1Ptr.findReductionPmfp()
 
 class Ait(NameTree):
     """
@@ -217,13 +245,23 @@ class AitCsOptions:
     degreeThreshold = 64
     """Node degree threshold. A node exceeding this threshold is labelled CS1."""
     fp2Threshold = 0.1
-    """BF2 false positive threshold."""
+    """BF2 false positive thresholds.
+       Either specify (low, high) limits as a tuple,
+       or specify (None, high, multipler) to compute low capacity limit as high - multiplier*degreeThreshold."""
     bf2Capacity = None
-    """BF2 capacity, specified as number of keys."""
+    """BF2 capacity, specified as number of keys. This precedes fp2Threshold.
+       Either specify (low, high) limits as a tuple,
+       or specify (None, high, multipler) to compute low capacity limit as high - multiplier*degreeThreshold."""
     fp1Threshold = 0.1
-    """BF1 false positive threshold."""
+    """BF1 false positive threshold.
+       Either specify (low, high) limits as a tuple,
+       or specify (None, high, multipler) to compute low capacity limit as high - multiplier*degreeThreshold."""
     bf1Capacity = None
-    """BF1 capacity, specified as number of keys."""
+    """BF1 capacity, specified as number of keys. This precedes fp1Threshold.
+       Either specify (low, high) limits as a tuple,
+       or specify (None, high, multipler) to compute low capacity limit as high - multiplier*degreeThreshold."""
+    lowLimitMultiplier = 2.0
+    """Parameter to compute capacity low limit in case it's unspecified."""
 
     def __init__(self, **params):
         for k, v in params.iteritems():
@@ -240,6 +278,28 @@ class AitCs:
         self.bf1 = nic.bf1
         self.bf2 = nic.bf2
         self.ait = Ait(self.bf1, self.bf2)
+
+        self.bf2Low, self.bf2High = self._computeLimits(self.bf2, "BF2", self.options.bf2Capacity, self.options.fp2Threshold)
+        self.bf1Low, self.bf1High = self._computeLimits(self.bf1, "BF1", self.options.bf1Capacity, self.options.fp1Threshold)
+
+    def _computeLimits(self, bf, bfLabel, capacityOption, thresholdOption):
+        if capacityOption is None:
+            if thresholdOption is None:
+                raise ValueError("either capacity or threshold must be specified for %s" % bfLabel)
+            return self._computeLimits(bf, bfLabel,
+                   [ None if x is None else x if i >= 2 else bf.capacity(x) for i,x in enumerate(thresholdOption) ], None)
+
+        if len(capacityOption) == 2:
+            low, high = capacityOption
+            if high - low < self.options.degreeThreshold:
+                sys.stderr.write("%s capacity limits %f, %f are closer than degreeThreshold" % (bfLabel, low, high))
+        elif len(capacityOption) == 3:
+            low, high, multiplier = capacityOption
+            low = high - multiplier * self.options.degreeThreshold
+        else:
+            raise ValueError("invalid %s threshold or capacity" % bfLabel)
+
+        return low, high
 
     def insert(self, name):
         # update AIT
@@ -279,18 +339,14 @@ class AitCs:
             parentNode.labelCs1()
 
         # FP2 reduction
-        bf2Capacity = self.bf2.capacity(self.options.fp2Threshold) if self.options.bf2Capacity is None \
-                      else self.options.bf2Capacity
-        while len(self.bf2) > bf2Capacity:
+        while len(self.bf2) > self.bf2High:
             target = self.ait.root.findReduction2()
             if target is None:
                 break
             target.labelCs1()
 
         # FP1 reduction
-        bf1Capacity = self.bf1.capacity(self.options.fp1Threshold) if self.options.bf1Capacity is None \
-                      else self.options.bf1Capacity
-        while len(self.bf1) > bf1Capacity:
+        while len(self.bf1) > self.bf1High:
             target = self.ait.root.findReduction1()
             if target is None:
                 break
@@ -299,17 +355,49 @@ class AitCs:
         self.ait.checkInvariants()
 
     def erase(self, name):
-        raise NotImplementedError
+        # update AIT
+        assert name in self.ait
+        node = self.ait[name]
+        assert node.inCs
+        node.inCs = False
+
+        erasedNodes = []
+        while node is not None:
+            if node.inCs or len(node.children) > 0:
+                break
+            erasedNodes.append(node)
+            node.unlabelCs2()
+            node.unlabelCs1()
+            parent = node.parent
+            del self.ait[node.name]
+            node = parent
+
+        # PMFP reduction
+        while len(self.ait) > 0 and len(self.bf2) < self.bf2Low and len(self.bf1) < self.bf1Low:
+            target = self.ait.root.findReductionPmfp()
+            if target is None:
+                break
+            target.labelCs2()
+            for child in target.children.itervalues():
+                child.labelCs1()
+
+        self.ait.checkInvariants()
 
 AitCs.Options = AitCsOptions
 
 if __name__ == "__main__":
     from nic import Nic
-    nic = Nic(128, 128)
-    options = AitCsOptions(useFreeFib1=False, bf2Capacity=8, bf1Capacity=8)
+    nic = Nic(128, 128, 0)
+    options = AitCsOptions(useFreeFib1=False, bf2Capacity=(5,8), bf1Capacity=(5,8))
     cs = AitCs(nic, options)
     print cs.ait
     names = [ "/".join(["", c1, c2, c3]) for c1 in "ABCD" for c2 in "abcd" for c3 in "1234" ]
     for name in names:
+        print "INS %s" % name
         cs.insert(name)
+        print cs.ait
+    names.reverse()
+    for name in names:
+        print "DEL %s" % name
+        cs.erase(name)
         print cs.ait
