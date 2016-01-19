9badc37e71255ca08608887684ce5d6ec8db2795
Junxiao Shi <git@mail1.yoursunny.com>
2016-01-19 22:35:46 +0000

    analyze: vary-ait-threshold.tsv bugfix

--- analyze/vary-ait-threshold.sh
+++ analyze/vary-ait-threshold.sh
@@ -22,21 +22,6 @@ for PARAM in "$@"; do
   PARAMS="$PARAMS \"${PARAM//\"/\\\"}\""
 done
 
-(
-  echo -n degree
-  echo -ne '\t'
-  echo -n fp2low
-  echo -ne '\t'
-  echo -n fp2high
-  echo -ne '\t'
-  echo -n fp1low
-  echo -ne '\t'
-  echo -n fp1high
-  echo -ne '\t'
-  echo -n $(echo | gawk -f $R/analyze/fp-classify.awk | head -1)
-  echo
-) > $KEY.vary-ait-threshold.tsv
-
 while read -r -a THRESHOLDS; do
   DEGREE=${THRESHOLDS[0]}
   FP2LOW=${THRESHOLDS[1]}
@@ -71,16 +56,40 @@ while read -r -a THRESHOLDS; do
   echo
 done < $THRESHOLDS_FILE | $R/analyze/parallelize.sh
 
+(
+  echo -n degree
+  echo -ne '\t'
+  echo -n fp2low
+  echo -ne '\t'
+  echo -n fp2high
+  echo -ne '\t'
+  echo -n fp1low
+  echo -ne '\t'
+  echo -n fp1high
+  echo -ne '\t'
+  echo -n freeFib1
+  echo -ne '\t'
+  echo -n $(echo | gawk -f $R/analyze/fp-classify.awk | head -1)
+  echo
+) > $KEY.vary-ait-threshold.tsv
+
 while read -r -a THRESHOLDS; do
   DEGREE=${THRESHOLDS[0]}
   FP2LOW=${THRESHOLDS[1]}
   FP2HIGH=${THRESHOLDS[2]}
   FP1LOW=${THRESHOLDS[3]}
   FP1HIGH=${THRESHOLDS[4]}
-  KEY1=$KEY.degree-${DEGREE//,/-}_fp2-${FP2LOW}-${FP2HIGH}_fp1-${FP1LOW}-${FP1HIGH}
+  if [[ ${THRESHOLDS[5]} == "FIB1" ]]; then
+    KEYFREEFIB1="freeFib1"
+    FREEFIB1="Y"
+  else
+    KEYFREEFIB1="noFib1"
+    FREEFIB1="N"
+  fi
+  KEY1=$KEY.degree-${DEGREE//,/-}_fp2-${FP2LOW}-${FP2HIGH}_fp1-${FP1LOW}-${FP1HIGH}_${KEYFREEFIB1}
 
   tail -n+2 $KEY1.fp-classify.tsv | \
-  sed -e "s/^/$DEGREE\t$FP2LOW\t$FP2HIGH\t$FP1LOW\t$FP1HIGH\t/"
+  sed -e "s/^/$DEGREE\t$FP2LOW\t$FP2HIGH\t$FP1LOW\t$FP1HIGH\t$FREEFIB1\t/"
 done < $THRESHOLDS_FILE >> $KEY.vary-ait-threshold.tsv
 
 column -t $KEY.vary-ait-threshold.tsv
