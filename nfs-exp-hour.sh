154c0c0daeb9e6d1f10e8a890c1b2715045537b2
Junxiao Shi <git@mail1.yoursunny.com>
2017-05-22 00:23:48 -0700

    Mininet script

---
+++ nfs-exp-hour.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Run NFS experiment in Mininet for one-hour trace.
+# This should be executed in a virtual machine.
+#   /data mounts this repository, read-write.
+#   /nfs-dataset mounts the NFS dataset, read-only.
+# Usage: /data/nfs-exp-hour.sh <hour>
+
+HOUR=$1
+NEXTHOUR=$(printf %02d $((HOUR+1)))
+HOUR=$(printf %02d $HOUR)
+
+export NFS_DATASET=/nfs-dataset
+export NFS_TIMEPERIOD=${HOUR}00-${NEXTHOUR}00
+export NFS_DURATION=3700
+export PYTHONPATH=$HOME/mnndn
+
+mkdir -p /data/nfs-$NFS_TIMEPERIOD
+
+sudo rm -rf /tmp/mnndn /tmp/nfs-traffic.txt
+cd $HOME/NDN-NIC/traffic
+./nfs.sh &> /data/nfs-$NFS_TIMEPERIOD/exp.log
+
+for H in $(ls /tmp/mnndn); do
+  cat /tmp/mnndn/$H/var/log/ndn/ttt.tsv.xz >/data/nfs-$NFS_TIMEPERIOD/$H.ttt.tsv.xz
+done
+cat /tmp/nfs-traffic.txt >/data/nfs-$NFS_TIMEPERIOD/nfs-traffic.txt
