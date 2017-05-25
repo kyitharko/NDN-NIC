3103401f50fa78ce26fce96bc5b8cbe476a52282
Junxiao Shi <git@mail1.yoursunny.com>
2017-05-25 13:13:00 -0700

    bfsize uses 3 hash functions

--- active.sh
+++ active.sh
@@ -5,6 +5,6 @@ export BF2EXTRA=,xor,../bf2.poly
 export BF3EXTRA=,xor,../bf3.poly
 export JOBS=4x1x10
 
-$NDNNICROOT/analyze/vary-ait-threshold.sh ../active.fpthres.tsv ../active.bfsizes.tsv
+$NDNNICROOT/analyze/vary-ait-threshold.sh ../active.fpthres.tsv ../active.bfsize.tsv
 
 # TODO tailored degree threshold
