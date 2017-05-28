79f6ac76d0dc8eecfe0fd9939fabfe45a061b70d
Junxiao Shi <git@mail1.yoursunny.com>
2017-05-27 20:02:10 -0700

    bfsize: packet-processing is now in one.sh

--- bfsize.sh
+++ bfsize.sh
@@ -11,7 +11,3 @@ $NDNNICROOT/analyze/vary-bf-size.sh basic ../bfsize.tsv --cs=BasicCs
 
 ACTIVEKEY=active_degree-64-64-32-16_fp2-2x-001_fp1-2x-001
 $NDNNICROOT/analyze/vary-bf-size.sh $ACTIVEKEY ../bfsize.tsv --cs="ActiveCs(nic,ActiveCs.Options(degreeThreshold=ActiveCs.DegreeThreshold(64,64,32,16),fp2Threshold=(None,0.001,2),fp1Threshold=(None,0.001,2)),trace=open('KEY.HOSTNAME.ait-trace.log','w'))"
-
-for KEY in direct basic $ACTIVEKEY; do
-  echo $NDNNICROOT/analyze/packet-processing.sh $KEY
-done | $NDNNICROOT/analyze/parallelize.sh
